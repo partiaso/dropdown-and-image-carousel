53b78832fadb116ad2c8f2f8b20b6106dc6ea49e
Party <57828409+partiaso@users.noreply.github.com>
2025-04-09 18:41:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# My Web Template
+
+This is a simple and customizable web project template that includes the essential tools for modern web development:
+
+- Webpack for bundling and serving files
+- ESLint for linting JavaScript code
+- Prettier for code formatting
+- A basic folder structure to get started quickly
+
+# Command Description
+
+- npm start -> Start development server with live reload
+- npm run build -> Build project for production into /dist
+- npm run lint -> Run ESLint on all files inside /src
+- npm run format -> Format all files in the project using Prettier
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+// webpack.config.js
+
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/index.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
